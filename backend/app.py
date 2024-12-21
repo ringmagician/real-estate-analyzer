eabd17e803900bad1cae06e89490ca1bb1f79a24
ringmagician <165564388+ringmagician@users.noreply.github.com>
2024-12-21 07:42:52 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from scraper import scrape_listings
+import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to port 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
+    
 app = Flask(__name__)
 CORS(app)  # Allow all origins
 
